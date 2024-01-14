e3e9043fd8f8d2d95a3a6ed52b3d17c9ae3ef6f8
Tim-E <caffeinated.ink@gmail.com>
2024-01-14 18:45:42 -0500

    Final exercise complete

--- errors-and-debugging/exercises/DebuggingLogicErrors5.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors5.js
@@ -6,10 +6,11 @@ let launchReady = false;
 let fuelLevel = 17000;
 let crewStatus = true;
 let computerStatus = 'green';
+let fuelStatus = false;
 
 if (fuelLevel >= 20000) {
    console.log('Fuel level cleared.');
-   launchReady = true;
+   fuelStatus = true;
 } else {
    console.log('WARNING: Insufficient fuel!');
    launchReady = false;
@@ -17,11 +18,11 @@ if (fuelLevel >= 20000) {
 
 console.log("launchReady = ", launchReady);
 
-if (crewStatus && computerStatus === 'green'){
-   console.log('Crew & computer cleared.');
+if (crewStatus && computerStatus === 'green' && fuelStatus === true){
+   console.log('Crew & computer cleared. Fuel is sufficient.');
    launchReady = true;
 } else {
-   console.log('WARNING: Crew or computer not ready!');
+   console.log('WARNING: Crew or computer not ready, or fuel is insufficient!');
    launchReady = false;
 }
 
