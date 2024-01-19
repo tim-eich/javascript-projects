8f8f2e723f146df6b762f76c4196e6cbbb1ba73e
Tim-E <caffeinated.ink@gmail.com>
2024-01-18 23:59:48 -0500

    Class 3 Studio

--- arrays/studio/array-string-conversion/array-testing.js
+++ arrays/studio/array-string-conversion/array-testing.js
@@ -8,27 +8,29 @@ strings = [protoArray1, protoArray2, protoArray3, protoArray4];
 //2) 
 function reverseCommas() {
 	//TODO: 1. create and instantiate your variables.
-	let check;
+	let check = strings[0];
 	let output;
 	//TODO: 2. write the code required for this step
-
+	if (check.includes(',')) {
+		output = check.split(',').reverse().join(',');
+	}
 	//NOTE: For the code to run properly, you must return your output.  this needs to be the final line of code within the function's { }.
 	return output;
 }
 
 //3)
 function semiDash() {
-	let check;
+	let check = strings[1];
 	let output;
 //TODO: write the code required for this step
-
+	if (check.)
   
 	return output;
 }
 
 //4)
 function reverseSpaces() {
-	let check;
+	let check = strings[2];
 	let output;
   //TODO: write the code required for this step
 
@@ -37,13 +39,16 @@ function reverseSpaces() {
 
 //5)
 function commaSpace() {
-	let check;
+	let check = strings[3];
 	let output;
 	//TODO: write the code required for this step
-  
+	if (check.includes(', ')) {
+		output = check.split(',').reverse().join(',');
+	}
 	return output;
 }
-
+console.log(reverseCommas())
+console.log(commaSpace());
 // NOTE: Don't add or modify any code below this line or your program might not run as expected.
 module.exports = {
 	strings : strings, 
--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -3,9 +3,29 @@ let str = "LaunchCode";
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
+let newStr = str.slice(3).concat(str.slice(0,3));
+console.log(newStr);
 
 //Use a template literal to print the original and modified string in a descriptive phrase.
+console.log(`The original string is ${str}. The modified string is ${newStr}.`);
+console.log()
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
+let number = input.question("Please enter a number: ");
+
+/* 
+// Checking input and word length
+console.log(`You entered: ${number}
+The word is ${str.length} characters long.`);
+*/
 
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+if (number > str.length) {
+    newStr = str.slice(3).concat(str.slice(0,3));
+    console.log(`You have entered ${number}, but the word is only ${str.length} characters long.
+The default value of 3 will be used.
+The original string was ${str}. The new string is ${newStr}`);
+} else {
+    newStr = str.slice(number).concat(str.slice(0,number));
+    console.log(`The original string was ${str}. The new string is ${newStr}`);
+}
