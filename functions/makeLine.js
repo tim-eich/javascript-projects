c99957e211d47677d540500860521eb678fecb16
Tim-E <caffeinated.ink@gmail.com>
2024-01-24 13:11:24 -0500

    Refactored code for bonus

--- functions/makeLine.js
+++ functions/makeLine.js
@@ -1,14 +1,20 @@
-function makeLine(size) {
+const input = require('readline-sync');
+
+function makeLine(size, newChar) {
     let count = 1;
     let line = '';
+    let char = '#';
+    if (newChar) {
+        char = newChar;
+    }
     while (count <= size) {
-        line += '#';
+        line += char;
         count ++;
     }
     return line;
 }
 
-// console.log(makeLine(7));
+// console.log(makeLine();
 
 function makeSquare(size) {
     let square = makeLine(size);
@@ -20,20 +26,34 @@ function makeSquare(size) {
 
 // console.log(makeSquare(5));
 
-function makeRectangle(width, height) {
-    let row = makeLine(width);
-    let rectangle = row
-    for (let i = 0; i < height - 1; i ++) {
-        rectangle += '\n' + row;
+function makeRectangle(width, height, newChar) {
+    if (newChar) {
+        let row = makeLine(width, newChar);
+        let rectangle = row
+        for (let i = 0; i < height - 1; i ++) {
+            rectangle += '\n' + row;
+        }
+        return rectangle;
+    } else {
+        let row = makeLine(width);
+        let rectangle = row
+        for (let i = 0; i < height - 1; i ++) {
+            rectangle += '\n' + row;
+        }
+        return rectangle;
     }
-    return rectangle;
 }
 
-// console.log(makeRectangle(9, 3));
+// console.log(makeRectangle(9, 3, 'n'));
 
-function makeDownwardStairs(height) {
+function makeDownwardStairs(height, newChar) {
     if (height <= 0) {
         return '';
+    } else if (newChar) {
+        stairs = newChar;
+        for (let i = 2; i <= height; i ++) {
+            stairs += '\n' + makeLine(i, newChar);
+        }
     } else {
         stairs = '#';
         for (let i = 2; i <= height; i ++) {
@@ -43,37 +63,53 @@ function makeDownwardStairs(height) {
     return stairs;
 }
 
-// console.log(makeDownwardStairs(6));
+// console.log(makeDownwardStairs(6, '*'));
 
-function makeSpaceLines(numSpaces, numChars) {
+function makeSpaceLines(numSpaces, numChars, newChar) {
     let spaces = '';
-    let chars = makeLine(numChars);
+    let chars = '';
+    if (newChar) {
+        chars = makeLine(numChars, newChar);
+    } else {
+        chars = makeLine(numChars);
+    }
     for (let i = 0; i < numSpaces; i ++) {
         spaces += ' ';
     }
     return spaces + chars + spaces;
 }
 
-// console.log(makeSpaceLines(6, 3));
+// console.log(makeSpaceLines(6, 5, '='));
 
-function makeIsoscelesTriangle(height) {
-    let triangle = makeSpaceLines(height - 1, 1);
-    for (let i = 1; i < height; i++) {
-        triangle += '\n' + makeSpaceLines(height - i - 1, 2 * i + 1);
+function makeIsoscelesTriangle(height, newChar) {
+    let triangle = '';
+    if (newChar) {
+        triangle = makeSpaceLines(height - 1, 1, newChar);
+        for (let i = 1; i < height; i++) {
+            triangle += '\n' + makeSpaceLines(height - i - 1, 2 * i + 1, newChar);
+        }
+        return triangle;
+    } else {
+        triangle = makeSpaceLines(height - 1, 1);
+        for (let i = 1; i < height; i++) {
+            triangle += '\n' + makeSpaceLines(height - i - 1, 2 * i + 1);
+        }
+        return triangle;
     }
-    return triangle;
 }
 
-// console.log(makeIsoscelesTriangle(6));
+// console.log(makeIsoscelesTriangle(6, '^'));
 
-function makeDiamond(height) {
-    let triangle = makeSpaceLines(height - 1, 1);
-    for (let i = 1; i < height; i++) {
-        triangle += '\n' + makeSpaceLines(height - i - 1, 2 * i + 1);
+function makeDiamond(height, newChar) {
+    let triangle = '';
+    if (newChar) {
+        triangle = makeIsoscelesTriangle(height, newChar);    
+    } else {
+        triangle = makeIsoscelesTriangle(height);
     }
     // Invert 'triangle' and place it one below existing 'triangle', creating a diamond.
     let diamond = triangle + '\n' + triangle.split('').reverse().join('');
     return diamond;
 }
 
-console.log(makeDiamond(5));
+// console.log(makeDiamond(5, '+'));
