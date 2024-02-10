3c76c4a2b4427a57ef8fdc0e28c08d25b87aa6ad
Tim-E <caffeinated.ink@gmail.com>
2024-02-10 01:42:53 -0500

    Class 9 Studio -in progress

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,44 @@
-
 let launchcode = {
-  
+    organization: 'nonprofit',
+    executiveDirector: 'Jeff',
+    percentageCoolEmployees: 100,
+    programsOffered: [
+        'JavaScript',
+        'Java',
+        'C++'
+    ]
 }
 
-module.exports = launchcode;
+function launchOutput(num) {
+    const replies = {
+        0: "Rutabagas! That doesn't work.",
+        3: 'Launch!', 
+        4: 'Code!',
+        6: 'Rocks!', 
+        7: 'LaunchCode!',
+        9: 'Launch Rocks!',
+        10: 'Code Rocks!',
+        13: 'LaunchCode Rocks!',
+    }
+    const factors = [2, 3, 5];
+    let replyCounter = 0;
+    for (let i = 0; i < factors.length; i++) {
+        console.log(i);
+    // check if number is a factor
+        if(num % factors[i] === 0) {
+            // use loop to log if number is divisible by 2, 3, or 5
+            replyCounter += factors[i] + 1;
+            console.log(replyCounter);
+        }
+    }
+    return replies[replyCounter];
+}
 
+console.log(launchOutput(30));
+
+// console.log(launchOutput(4));
+
+module.exports = {
+    launchcode: launchcode,
+    launchOutput: launchOutput
+}
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -4,5 +4,27 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
-  
+  test("should return 'nonprofit'", function() {
+    expect(launchcode.launchcode.organization).toBe("nonprofit");
+  });
+
+  test("should return true for a single letter", function() {
+    expect(launchcode.launchcode.executiveDirector).toBe("Jeff");
+  });
+
+  test("should confirm LC is 100% cool", function() {
+    expect(launchcode.launchcode.percentageCoolEmployees).toBe(100);
+  });
+
+  test("should list the names and number of programs offered", function() {
+    expect(launchcode.launchcode.programsOffered[0]).toBe('JavaScript');
+    expect(launchcode.launchcode.programsOffered[1]).toBe('Java');
+    expect(launchcode.launchcode.programsOffered[2]).toBe('C++');
+    expect(launchcode.launchcode.programsOffered.length).toEqual(3);
+  });
+
+  test("Should return 'Launch!' when passed a number that is ONLY divisible by 2", function() {
+    expect(launchcode.launchOutput(2)).toBe("Launch!");
+  });
 });
+
