ba03d8169865f37666814bd79b96d8de5a77ce2a
Tim-E <caffeinated.ink@gmail.com>
2024-02-24 11:35:50 -0500

    in-progress commit

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,24 +1,59 @@
 function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
-    let lab = labs[i];
-    let result = lab.runLab(3);
-    console.log(`${lab.student} code worked: ${result === 27}`);
+      let lab = labs[i];
+      let result = lab.runLab(3);
+      try {
+        console.log(`${lab.student} code worked: ${result === 27}`);
+      } catch(err) {
+        console.log("Error thrown.");
+      }
   }
 }
 
 let studentLabs = [
   {
-    student: 'Carly',
-    runLab: function (num) {
+      student: 'Carly',
+      runLab: function (num) {
         return Math.pow(num, num);
-    }
+      }
   },
   {
-    student: 'Erica',
-    runLab: function (num) {
+      student: 'Erica',
+      runLab: function (num) {
         return num * num;
-    }
+      }
   }
 ];
 
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+
 gradeLabs(studentLabs);
+gradeLabs(studentLabs2);
+
+
+// try {
+//   console.log('animal at index:', animals[index].name);
+// } catch(err) {
+//   console.log("We caught a TypeError, but our program continues to run!");
+//   console.log("You tried to access an animal at index:", index);
+// }
