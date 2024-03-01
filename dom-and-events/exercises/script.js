010263aab8097a582c690ebc337f9a436cae7347
Tim-E <caffeinated.ink@gmail.com>
2024-03-01 11:56:13 -0500

    Dom-and-events exercises complete

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,10 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click', event => {
+        document.getElementById('statusReport').innerHTML = "Houston, we have liftoff!";
+    });
 }
 
 window.addEventListener("load", init);
+
