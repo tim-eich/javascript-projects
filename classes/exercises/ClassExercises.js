1d33e23117c3f828d804ccc662974aaf192aaff0
Tim-E <caffeinated.ink@gmail.com>
2024-03-05 16:08:16 -0500

    Class exercises complete

--- classes/exercises/ClassExercises.js
+++ classes/exercises/ClassExercises.js
@@ -1,40 +1,40 @@
 // Define your Book class here:
 class Book {
-    constructor(title, author, copyrightDate, isbn, numPages, timesCheckedOut, discarded = 'no') {
+    constructor(title, author, copyrightDate, isbn, numPages, hasBeenCheckedOut, discarded = 'No') {
         this.title = title;
         this.author = author;
         this.copyrightDate = copyrightDate;
         this.isbn = isbn;
-        this.timesCheckedOut = timesCheckedOut;
+        this.hasBeenCheckedOut = hasBeenCheckedOut;
         this.numPages = numPages;
         this.discarded = discarded;
     }
-    checkoutBook() {
-        this.timesCheckedOut += 1;
+    checkout(checked = 1) {
+        this.hasBeenCheckedOut += checked;
     }
 }
 
 // Define your Manual and Novel classes here:
 class Novel extends Book {
-    constructor() {
-        super();
+    constructor(title, author, copyright, isbn, pages, timesCheckedOut, discarded){
+       super(title, author, copyright, isbn, pages, timesCheckedOut, discarded);
     }
-    // checkForDiscard() {
-    //     if (this.timesCheckedOut > 100) {
-    //         this.discarded = 'yes';
-    //     }
-    // } 
+    discard() {
+        if (this.hasBeenCheckedOut > 100) {
+            this.discarded = 'Yes';
+        }
+    } 
 }
 
 class Manual extends Book {
-    constructor() {
-        super();
+    constructor(title, author, copyright, isbn, pages, timesCheckedOut, discarded){
+       super(title, author, copyright, isbn, pages, timesCheckedOut, discarded);
     }
-    // checkForDiscard(currentYear) {
-    //     if (currentYear - this.copyrightDate > 5) {
-    //         this.discarded = 'yes';
-    //     }
-    // }
+    discard(currentYear) {
+        if (currentYear - this.copyrightDate > 5) {
+            this.discarded = 'Yes';
+        }
+    }
 }
 
 // Declare the objects for exercises 2 and 3 here:
@@ -43,4 +43,8 @@ let prideAndPrejudice = new Novel('Pride and Prejudice', 'Jane Austen', 1813, '1
 let shuttleBuilding = new Manual('Top Secret Shuttle Building Manual', 'Redacted', 2013, '0000000000000', 1147, 1, 'no');
 
 // Code exercises 4 & 5 here:
-console.log(shuttleBuilding);
+prideAndPrejudice.checkout(80);
+prideAndPrejudice.discard();
+
+console.log(prideAndPrejudice);
+
